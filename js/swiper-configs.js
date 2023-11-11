918e0dc02a5ab585e132f1e6a287be558b771199
greentea777 <117492457+greentea777@users.noreply.github.com>
2023-11-10 16:02:23 -0800

    completed home page content display except for the product section

---
+++ js/swiper-configs.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoHeight: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+});
+
+const swiperHeroSection = new Swiper(".swiper-hero-section", {
+  loop: true,
+  autoHeight: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+});
