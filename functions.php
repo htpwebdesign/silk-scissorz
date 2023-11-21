c6d26c0e05c359808dc0a5d38c4ab2519aea190c
Kaori Sato <ksato9@my.bcit.ca>
2023-11-21 14:25:23 -0800

    changed API key

--- functions.php
+++ functions.php
@@ -237,7 +237,7 @@ function silk_excerpt_more($more)
 add_filter('excerpt_more', 'silk_excerpt_more');
 
 function my_acf_google_map_api( $api ){
-    $api['key'] = 'AIzaSyDXkLNbXBmazlq41GVLYY5_9ivOAd3cNuA';
+    $api['key'] = 'AIzaSyAbDccbZ61WQAxDVzfGWOBdsahhN5AHT10';
     return $api;
 }
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
