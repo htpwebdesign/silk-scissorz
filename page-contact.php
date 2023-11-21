3c9d7eb4a3f519d7b397ee599a53df681e246fc8
greentea777 <117492457+greentea777@users.noreply.github.com>
2023-11-21 12:18:01 -0800

    remove map function on contact page

--- page-contact.php
+++ page-contact.php
@@ -17,108 +17,100 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-    <?php while (have_posts()) : the_post(); ?>
+  <?php while (have_posts()) : the_post(); ?>
 
     <h1><?php the_title(); ?></h1>
     <?php
-		if (function_exists('get_field')) :
-		?>
-    <section>
+    if (function_exists('get_field')) :
+    ?>
+      <section>
         <?php
-				/* === Contact Info === */
-				if (have_rows('contact_info')) :
-				?>
-        <article>
+        /* === Contact Info === */
+        if (have_rows('contact_info')) :
+        ?>
+          <article>
             <h2>Contact Info</h2>
             <?php
-						while (have_rows('contact_info')) : the_row();
-							$store_address = get_sub_field('store_address');
-							$store_phone = get_sub_field('store_phone');
-							$store_email = get_sub_field('store_email');
-							$store_parking_info = get_sub_field('store_parking_info');
-						?>
-            <address>
+            while (have_rows('contact_info')) : the_row();
+              $store_address = get_sub_field('store_address');
+              $store_phone = get_sub_field('store_phone');
+              $store_email = get_sub_field('store_email');
+              $store_parking_info = get_sub_field('store_parking_info');
+            ?>
+              <address>
                 <p><?php echo esc_html($store_address); ?></p>
                 <a href="tel:<?php echo esc_html($store_phone); ?>"><?php echo esc_html($store_phone); ?></a>
                 <a href="mailto:<?php echo esc_html($store_email); ?>"><?php echo esc_html($store_email); ?></a>
-            </address>
+              </address>
             <?php
-						endwhile;
-						?>
-        </article>
+            endwhile;
+            ?>
+          </article>
         <?php
-				endif;
+        endif;
 
-        
-				/* === Business Hours === */
-				if (have_rows('business_hours')) : ?>
-        <table>
+
+        /* === Business Hours === */
+        if (have_rows('business_hours')) : ?>
+          <table>
             <caption>
-                Business Hours
+              Business Hours
             </caption>
             <thead>
-                <?php
-							$day_of_week_label = get_sub_field_object('day_of_week');
-							$open_time_label = get_sub_field_object('opening_hour');
-							$close_time_label = get_sub_field_object('closing_hour');
-							?>
-                <tr>
-                    <th><?php echo esc_html($day_of_week_label['label']); ?></th>
-                    <th><?php echo esc_html($open_time_label['label']); ?></th>
-                    <th><?php echo esc_html($close_time_label['label']); ?></th>
-                </tr>
+              <?php
+              $day_of_week_label = get_sub_field_object('day_of_week');
+              $open_time_label = get_sub_field_object('opening_hour');
+              $close_time_label = get_sub_field_object('closing_hour');
+              ?>
+              <tr>
+                <th><?php echo esc_html($day_of_week_label['label']); ?></th>
+                <th><?php echo esc_html($open_time_label['label']); ?></th>
+                <th><?php echo esc_html($close_time_label['label']); ?></th>
+              </tr>
             </thead>
             <tbody>
-                <?php
-							while (have_rows('business_hours')) : the_row();
-								$day_of_week = get_sub_field('day_of_week');
-								$open_time = get_sub_field('opening_hour');
-								$close_time = get_sub_field('closing_hour');
-								// if $open_time and $close_time are empty, first td print day, second prints closed, only two tds
-								if (empty($open_time) && empty($close_time)) :
-							?>
-                <tr>
+              <?php
+              while (have_rows('business_hours')) : the_row();
+                $day_of_week = get_sub_field('day_of_week');
+                $open_time = get_sub_field('opening_hour');
+                $close_time = get_sub_field('closing_hour');
+                // if $open_time and $close_time are empty, first td print day, second prints closed, only two tds
+                if (empty($open_time) && empty($close_time)) :
+              ?>
+                  <tr>
                     <td><?php echo esc_html($day_of_week); ?></td>
                     <td colspan="2">Closed</td>
-                </tr>
+                  </tr>
                 <?php
-								else :
-								?>
-                <tr>
+                else :
+                ?>
+                  <tr>
                     <td><?php echo esc_html($day_of_week); ?></td>
                     <td><?php echo esc_html($open_time); ?></td>
                     <td><?php echo esc_html($close_time); ?></td>
-                </tr>
-                <?php
-								endif;
-							endwhile;
-							?>
+                  </tr>
+              <?php
+                endif;
+              endwhile;
+              ?>
             </tbody>
-        </table>
+          </table>
         <?php
-				endif;
-				?>
-    </section>
-    <section class="map-section">
+        endif;
+        ?>
+      </section>
+      <section class="map-section">
         <h2>Parking Info</h2>
         <p><?php echo esc_html($store_parking_info); ?></p>
         <div class="map">
-            <!-- Map -->
+          <!-- Map -->
         </div>
-    </section>
-    <?php the_content(); ?>
-    <?php
-		endif;
-	endwhile; // end of the loop. 
-	?>
-    <?php
-	function my_acf_google_map_api($api)
-	{
-		$api['key'] = 'store_location_map';
-		return $api;
-	}
-	add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
-	?>
+      </section>
+      <?php the_content(); ?>
+  <?php
+    endif;
+  endwhile; // end of the loop. 
+  ?>
 
 </main><!-- #main -->
 
