6c4ffac47a57243e7cd2b3474f954e4d1f1f133f
Jadie Lin <jadie.lin.edu@gmail.com>
2023-11-29 14:11:26 -0800

    add faq all categories selector and add margin

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("categories").addEventListener("change", function () {
     let selectedCategory = this.value;
     let allCategories = document.querySelectorAll(".category-content");
+    let allButton = document.getElementById("all-categories");
+
+    if (selectedCategory == allButton.value){
+      allCategories.forEach(function (content) {
+      content.style.display = "block";});
+    }else{
     allCategories.forEach(function (content) {
       content.style.display = "none";
     });
-    document.getElementById(selectedCategory).style.display = "block";
+    document.getElementById(selectedCategory).style.display = "block";}
   });
+
+  
 });
