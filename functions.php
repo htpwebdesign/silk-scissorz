27079ef04650154b7dff9075a6ec39d01b6f4fbd
KhushiMangla <Khushimangla99@gmail.com>
2023-11-30 15:04:55 -0800

    customized the dashboard widget

--- functions.php
+++ functions.php
@@ -259,3 +259,25 @@ function new_loop_shop_per_page( $cols ) {
  * Change number of products that are displayed per page (shop page)
  */
 add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page');
+
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widget');
+function my_custom_dashboard_widget() {
+global $wp_meta_boxes;
+wp_add_dashboard_widget('custom_video_widget', 'Video Tutorial', 'custom_dashboard_fun');
+}
+
+function custom_dashboard_fun() {
+	echo '<a href="https://gearoid.me/pokemon/" target="_blank">See the tutorial</a>';
+	}
+
+	function wporg_remove_all_dashboard_metaboxes() {
+		// Remove the rest of the dashboard widgets
+		remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+		remove_meta_box( 'health_check_status', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
+		remove_meta_box('woocommerce_dashboard_status', 'dashboard', 'normal');
+    	remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
+	}
+	add_action( 'wp_dashboard_setup', 'wporg_remove_all_dashboard_metaboxes' );
